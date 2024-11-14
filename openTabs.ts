207204cfe0b79d9599da2e234cda5f9e94d5f250
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2024-11-14 09:02:18 +0100

    Reintroduced airport mappings Added more console logs Better way of noting rotations (for cities)

--- openTabs.ts
+++ openTabs.ts
@@ -15,9 +15,61 @@ type FlightDate = {
 
 let cheapestFlightPrices: CheapestFlightPrice[] = [];
 let cheapestFlightPriceFoundUrl: string = "";
-const aircraftModel = "787";
+const aircraftModel = "A350";
 
-const saturday = new Date("2024-08-31");
+const cityCodesForMultipleAirports = {
+  BJS: ["PEK", "PKX"],
+  BHZ: ["CNF", "PLU"],
+  BUH: ["OTP", "BBU"],
+  BUE: ["EZE", "AEP"],
+  CHI: ["ORD", "MDW", "RFD"],
+  JKT: ["CGK", "HLP"],
+  LON: ["LHR", "LGW", "LCY", "STN", "LTN", "SEN"],
+  MIL: ["MXP", "LIN", "BGY"],
+  YMQ: ["YUL", "YMX", "YHU"],
+  MOW: ["SVO", "DME", "VKO", "ZIA", "OSF"],
+  NYC: ["JFK", "LGA", "EWR"],
+  OSA: ["ITM", "KIX", "UKB"],
+  PAR: ["ORY", "CDG", "LBG", "BVA"],
+  RIO: ["GIG", "SDU"],
+  ROM: ["FCO", "CIA"],
+  SAO: ["CGH", "GRU", "VCP"],
+  SPK: ["CTS", "OKD"],
+  SEL: ["ICN", "GMP"],
+  STO: ["ARN", "BMA", "NYO", "VST"],
+  TCI: ["TFN", "TFS"],
+  TYO: ["HND", "NRT"],
+  YTO: ["YYZ", "YTZ", "YHM", "YKF"],
+  WAS: ["IAD", "DCA", "BWI"],
+  ALA: ["ALA", "BXJ"],
+  BKK: ["BKK", "DMK", "BKK"],
+  BFS: ["BFS", "BHD"],
+  CTU: ["CTU", "TFU", "HZU"],
+  CMB: ["CMB", "RML"],
+  DKR: ["DKR", "DSS"],
+  DFW: ["DFW", "DAL", "FTW", "AFW", "ADS"],
+  DXB: ["DXB", "DWC"],
+  GLA: ["GLA", "PIK"],
+  HOU: ["HOU", "IAH", "EFD"],
+  IST: ["IST", "SAW", "ISL"],
+  JNB: ["JNB", "HLA"],
+  KUL: ["KUL", "SZB", "KUL"],
+  IEV: ["IEV", "KBP"],
+  LAX: ["LAX", "SBD", "ONT", "SNA", "VNY", "PMD", "LGB", "BUR"],
+  MDE: ["MDE", "EOH"],
+  MEX: ["MEX", "NLU"],
+  MEL: ["MEL", "MEB", "AVV"],
+  MIA: ["MIA", "FLL", "PBI"],
+  NGO: ["NGO", "NKM"],
+  SAN: ["SAN", "TIJ"],
+  SFO: ["SFO", "OAK", "SJC", "STS"],
+  SEA: ["SEA", "BFI", "PAE"],
+  SHA: ["PVG", "SHA"],
+  TPE: ["TPE", "TSA"],
+  THR: ["IKA", "THR"],
+};
+
+const saturday = new Date("2024-11-30");
 let saturdayIso = saturday.toISOString().substring(0, 10);
 
 const userAgent = new UserAgent({ deviceCategory: "desktop" });
@@ -75,13 +127,38 @@ async function prepareUrls(
       !restrictedAirportCodes.includes(airportRotation.split("-")[1])
   );
 
+  const airportRotationsSet = new Set<string>();
+
   for (const airportRotation of filteredAirportRotations) {
-    const linkAndAirportRotationObj = {
-      url: `https://www.kayak.ie/flights/${airportRotation}/${saturdayIso}-flexible-1day?fs=eqmodel=~${aircraftModel};stops=~0&sort=price_a`,
-      airportRotation: airportRotation,
-    };
-    urlsToOpen.push(linkAndAirportRotationObj);
+    let splitAirportRotation = airportRotation.split("-");
+
+    const airportToCityMap = {};
+
+    Object.entries(cityCodesForMultipleAirports).forEach(
+      ([cityCode, airportCodes]) => {
+        airportCodes.forEach((airportCode) => {
+          airportToCityMap[airportCode] = cityCode;
+        });
+      }
+    );
+
+    splitAirportRotation.forEach((airportCode, index, array) => {
+      if (airportToCityMap[airportCode]) {
+        array[index] = airportToCityMap[airportCode];
+      }
+    });
+
+    airportRotationsSet.add(splitAirportRotation.join("-"));
   }
+
+  airportRotationsSet.forEach((rotation) => {
+    const linkAndAirportRotationObj = {
+      url: `https://www.kayak.ie/flights/${rotation}/${saturdayIso}-flexible-1day?fs=eqmodel=~${aircraftModel};stops=~0&sort=price_a`,
+      airportRotation: rotation,
+    };
+
+    urlsToOpen.push(linkAndAirportRotationObj);
+  });
 }
 
 async function lookForSingleFlights(
@@ -134,6 +211,9 @@ async function lookForSingleFlights(
 
     const cheapestFlightPrice = await getCheapestFlightPrice(page);
     if (cheapestFlightPrice !== null && cheapestFlightPrice !== undefined) {
+      console.log(
+        "Prices have been found for this flight. Starting to process date combinations..."
+      );
       cheapestFlightPriceFoundUrl = url.url;
 
       await processDateCombinations(
@@ -168,7 +248,7 @@ async function processDateCombinations(
     const firstMidpoint = midpoints[0];
     const secondMidpoint = midpoints[1];
 
-    const url = `https://www.kayak.ie/flights/BEG,TSR,KVO-${firstMidpoint}/${dateCombination.departureDate}/${airportRotation}/${dateCombination.midpointDate}/${secondMidpoint}-BEG,TSR,KVO/${dateCombination.returnDate}?fs=baditin=baditin;virtualinterline=-virtualinterline;eqmodel=~${aircraftModel}&sort=price_a`;
+    const url = `https://www.kayak.ie/flights/BEG-${firstMidpoint}/${dateCombination.departureDate}/${airportRotation}/${dateCombination.midpointDate}/${secondMidpoint}-BEG/${dateCombination.returnDate}?sort=price_a`;
     urlsToOpenForCombinations.push(url);
   }
 
@@ -223,6 +303,7 @@ async function processDateCombinations(
     if (cheapestFlightPrice !== null && cheapestFlightPrice !== undefined) {
       const priceObj = createPriceObject(cheapestFlightPrice, url);
       cheapestFlightPrices.push(priceObj);
+      console.log("Added the link.")
     }
 
     await page.close();
@@ -232,6 +313,10 @@ async function processDateCombinations(
     cheapestFlightPrices.sort((a, b) => a.price - b.price);
     await sendCheapestPricesEmail(cheapestFlightPrices);
     cheapestFlightPrices.length = 0;
+  } else {
+    console.log(
+      "Wasn't able to find any prices for these combinations. Moving on..."
+    );
   }
 }
 
